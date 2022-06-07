9afc154652cf7bc51877b05ffcfd35c206bd7e47
Andrew <x.and.i.rew.e@gmail.com>
2022-06-07 21:29:02 +1000

    updated sudo permissions

--- setup.sh
+++ setup.sh
@@ -2,8 +2,13 @@
 
 # Instructions from https://wiki.t2linux.org/guides/dkms/
 
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
 # Install BCE (Buffer Copy Engine) module
-sudo git clone https://github.com/t2linux/apple-bce-drv /usr/src/apple-bce-0.2
+git clone https://github.com/t2linux/apple-bce-drv /usr/src/apple-bce-0.2
 cat > /usr/src/apple-bce-0.2/dkms.conf << EOF
 PACKAGE_NAME="apple-bce"
 PACKAGE_VERSION="0.2"
@@ -13,19 +18,19 @@ BUILT_MODULE_NAME[0]="apple-bce"
 DEST_MODULE_LOCATION[0]="/kernel/drivers/misc"
 AUTOINSTALL="yes"
 EOF
-sudo dkms install -m apple-bce -v 0.2
+dkms install -m apple-bce -v 0.2
 
 # Install Touchbar and Ambient Light sensor modules
-sudo git clone https://github.com/Redecorating/apple-ib-drv /usr/src/apple-ibridge-0.1
-sudo dkms install -m apple-ibridge -v 0.1
+git clone https://github.com/Redecorating/apple-ib-drv /usr/src/apple-ibridge-0.1
+dkms install -m apple-ibridge -v 0.1
 
 # Load kernel modules
-sudo modprobe apple_bce
-sudo modprobe apple_ib_tb
-sudo modprobe apple_ib_als
+modprobe apple_bce
+modprobe apple_ib_tb
+modprobe apple_ib_als
 
 # load on boot
-echo apple-bce | sudo tee /etc/modules-load.d/t2.conf
+echo apple-bce | tee /etc/modules-load.d/t2.conf
 
 # load on early boot
 cat <<EOF >> /etc/initramfs-tools/modules
@@ -37,4 +42,4 @@ EOF
 
 # Setup Touchbar
 bash <(curl -s https://wiki.t2linux.org/tools/touchbar.sh)
-echo "1" | sudo touchbar
+echo "1" | touchbar
